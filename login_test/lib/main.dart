833d5c7231ef46ad4515a869db588603abcb7138
tharakaWijesundara <wmtharaka98@gmail.com>
2020-03-09 08:38:05 +0530

    first

---
+++ login_test/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:login_test/Screens/login_screen.dart';
+import 'package:login_test/Screens/waiting.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:login_test/Home/homePage.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Login UI',
+      debugShowCheckedModeBanner: false,
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (context,AsyncSnapshot<FirebaseUser> snapshot) {
+        if(snapshot.connectionState == ConnectionState.waiting)
+          return SplashPage();
+        if(!snapshot.hasData || snapshot.data == null)
+          return LoginScreen();
+        return HomePage();
+      },
+    );
+  }
+}
